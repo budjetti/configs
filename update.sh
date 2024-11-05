900c81b1442fdd8899a28816578e5f3d1e8e0884
budjetti <jaakko.ojala@live.fi>
2024-11-05 18:29:51 +0200

    improved logging and todos

--- update.sh
+++ update.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-echo "hello jon"
 for var in "$@"
 do
 	# TODO overengineer this with structs
-	echo "$var"
-	if [ $var == "i3" -o $var == "all" ]; then
-		cp i3/config ~/.config/i3/config
-		echo "i3 needs to be restarted"
-	fi
 	if [ $var == "bash" -o $var == "all" ]; then
 		cp  bash/.bashrc ~/.bashrc
 		source ~/.bashrc
+		echo "Updated and reloaded bashrc"
+	fi
+	if [ $var == "i3" -o $var == "all" ]; then
+		cp i3/config ~/.config/i3/config
+		echo "Updated i3/config ... NEEDS RESTART"
 	fi
 	if [ $var == "nvim" -o $var == "neovim" -o $var == "all" ]; then
 		cp neovim/init.lua ~/.config/nvim/init.lua
-		echo "nvim may need be restarted"
+		echo "Updated nvim/init.lua ... NEEDS RESTART"
 	fi
+	# TODO vencord
+	# TODO hypr
+	# TODO waybar
 	if [ $var == "all" ]; then
 		break
 	fi

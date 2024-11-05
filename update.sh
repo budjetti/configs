ea5ea91d4937f9ffb72b816f683a7a83ab471cac
budjetti <jaakko.ojala@live.fi>
2024-11-05 17:01:26 +0200

    add update.sh

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "hello jon"
+for var in "$@"
+do
+	# TODO overengineer this with structs
+	echo "$var"
+	if [ $var == "i3" -o $var == "all" ]; then
+		cp i3/config ~/.config/i3/config
+		echo "i3 needs to be restarted"
+	fi
+	if [ $var == "bash" -o $var == "all" ]; then
+		cp  bash/.bashrc ~/.bashrc
+		source ~/.bashrc
+	fi
+	if [ $var == "all" ]; then
+		break
+	fi
+done

195955be53b6ca14b94845fe11a9a31906a63258
budjetti <jaakko.ojala@live.fi>
2024-11-05 19:05:07 +0200

    add diff check

--- update.sh
+++ update.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
+try_copy(){
+	DIFF=$(diff $1 $2)
+	if [ "$DIFF" == "" ] 
+	then
+		echo "$2 is already up to date"
+		return
+	else
+		cp $1 $2
+		echo -n "Updated $2"
+	fi
+
+	# Check for the "-r" at the end. There's probably a cleaner way to do this
+	if [ ! -z $3 ]; then
+		echo " ... NEEDS RESTART"
+	else
+		echo ""
+	fi
+}
+
 for var in "$@"
 do
 	# TODO overengineer this with structs
 	if [ $var == "bash" -o $var == "all" ]; then
-		cp  bash/.bashrc ~/.bashrc
+		try_copy bash/.bashrc ~/.bashrc
 		source ~/.bashrc
-		echo "Updated and reloaded bashrc"
 	fi
 	if [ $var == "i3" -o $var == "all" ]; then
-		cp i3/config ~/.config/i3/config
-		echo "Updated i3/config ... NEEDS RESTART"
+		try_copy i3/config ~/.config/i3/config -r
 	fi
 	if [ $var == "nvim" -o $var == "neovim" -o $var == "all" ]; then
-		cp neovim/init.lua ~/.config/nvim/init.lua
-		echo "Updated nvim/init.lua ... NEEDS RESTART"
+		try_copy neovim/init.lua ~/.config/nvim/init.lua -r
 	fi
 	# TODO vencord
 	# TODO hypr

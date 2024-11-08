316431ade0a0bf0c171c8af526564d8e1bfcddb8
Jaakko Ojala <jaakko.ojala@live.fi>
2024-11-08 17:44:29 +0200

    kickstart.nvim install script

--- kickstart.sh
+++ kickstart.sh
@@ -1,11 +1,10 @@
-# Install kickstart.nvim before copying init.lua
+#!/bin/bash
+
+# Install kickstart and its dependencies, then copy init.lua from repo
 
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
-
 sudo apt update
-
 sudo apt install make gcc ripgrep unzip git xclip neovim
-
 git clone https://github.com/nvim-lua/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
-
+cp init.lua ~/.config/nvim/init.lua
 nvim

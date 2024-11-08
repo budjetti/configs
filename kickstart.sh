8b2547f0ac085b061efc515fee6511cb297ab7c5
Jaakko Ojala <jaakko.ojala@live.fi>
2024-11-08 17:51:10 +0200

    fix init.lua path

--- kickstart.sh
+++ kickstart.sh
@@ -6,5 +6,5 @@ sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt update
 sudo apt install make gcc ripgrep unzip git xclip neovim
 git clone https://github.com/nvim-lua/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
-cp init.lua ~/.config/nvim/init.lua
+cp neovim/init.lua ~/.config/nvim/init.lua
 nvim

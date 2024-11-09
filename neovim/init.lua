b2c4f9b6225afa474a001538d22d7831bf2dfd75
Jaakko Ojala <jaakko.ojala@live.fi>
2024-11-09 14:16:10 +0200

    (){}[] autocomplete

--- neovim/init.lua
+++ neovim/init.lua
@@ -227,6 +227,13 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
   {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = true,
+    -- use opts = {} for passing setup options
+    -- this is equivalent to setup({}) function
+  },
+  {
     'numToStr/Comment.nvim',
     opts = {
       -- add any options here
